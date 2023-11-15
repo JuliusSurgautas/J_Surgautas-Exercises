8bf2f1fb87de6999402b267bfd84cb1afce882bd
J Surgautas-Exercises <144884734+JuliusSurgautas@users.noreply.github.com>
2023-11-15 11:24:23 +0200

    Add files via upload

---
+++ PD5/js/main.js
@@ -0,0 +1,72 @@
+// let mynumber = '1 num';
+// let mybigint='2 bigint';
+// let mystring='3string';
+// let myboolean='4bolean';
+// let myundefined=undefined;
+// let mynull=null;
+// let myobject='7 objects'
+
+
+// console.log(mynumber);
+// console.log(mybigint);
+// console.log(mystring);
+// console.log(myboolean);
+// console.log(myundefined);
+// console.log(mynull);
+// console.log(myobject);
+
+
+// let number =12;
+// console.log(typeof number);
+
+// let otherNumber=45.89;
+// console.log(typeof otherNumber);
+
+// let textOne='Beautiful Javascript World';
+// console.log(typeof textOne);
+
+// let textTwo='Beautiful Javascript World';
+// console.log(typeof textTwo);
+
+// let textThree='Beautiful Javascript World';
+// console.log(typeof textThree);
+
+// let isTrue=(true==false);
+// console.log(typeof isEqual);
+
+// let petName=undefined;
+// console.log(typeof petName);
+
+// let petAge=null;
+// console.log(typeof petAge);
+
+// let human={
+//     humanName:undefined,
+//     humanAge:null
+// };
+// console.log(typeof human);
+
+let myNumber= 10;
+console.log(myNumber);
+
+
+let myBigInt= 10n;
+console.log(typeof myBigInt);
+
+let myString= 11;
+console.log(typeof myString);
+
+let myboolean= 11n;
+console.log(typeof myboolean);
+
+let myUndefined= 12;
+console.log(typeof myUndefined);
+
+let mynull= 12n;
+console.log(typeof mynull);
+
+let myobject= 13;
+console.log(typeof myobject);
+
+
+
