222a8d0894e27eb080ffbfa5c180374f1e7e94de
J Surgautas-Exercises <144884734+JuliusSurgautas@users.noreply.github.com>
2024-05-02 15:33:03 +0300

    15 padaryta iki 15/3 toliau pabaigti.

---
+++ Pd15/15/1/js/main.js
@@ -0,0 +1,9 @@
+let Array = [1,2,3,5,7,10];
+let number=20;
+
+if(Array.includes(number)) {
+    console.log("Kintamasis masyvas");
+}
+else{
+    console.log("Kintamasis ne masyvas");
+};
---
+++ Pd15/15/2/js/main.js
@@ -0,0 +1,10 @@
+const numberArray= [1,2,3,45,78,120];
+const copiedArray=[];
+
+for(let i =0; i < numberArray.length; i++){
+    copiedArray[i]=numberArray[i];
+}
+
+
+console.log(numberArray);
+console.log(copiedArray)
